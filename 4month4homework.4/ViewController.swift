b475bf7aea77235af8e51299f1d6acf6d182f7b4
arbudu akram <arbudu05@gmail.com>
2023-01-14 18:14:09 +0600

    Initial Commit

---
+++ 4month4homework.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4month4homework.4
+//
+//  Created by акрам on 14/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
